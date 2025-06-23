dfe6863b5443d1d5f4bab4cb70c60296c002f9b5
Valerio Battaglia <vabatta@gmail.com>
2025-06-23 12:55:29 +0200

    fix format and tests

--- scripts/mkcd/mkcd_comp.zsh
+++ scripts/mkcd/mkcd_comp.zsh
@@ -1,15 +1,14 @@
 #compdef mkcd
 
 _mkcd() {
-    local context state line
-    typeset -A opt_args=()
+	local context state line
+	typeset -A opt_args=()
 
-    _arguments -C \
-        '(-h --help)'{-h,--help}'[Show help message for mkcd]' \
-        '--builtin[Use builtin mkdir and cd commands, bypassing any aliases]' \
-        '*:mkdir-args:->mkdir'
+	_arguments -C \
+		'(-h --help)'{-h,--help}'[Show help message for mkcd]' \
+		'*:mkdir-args:->mkdir'
 
-    if [[ $state == mkdir ]]; then
-        $_comps[mkdir]
-    fi
+	if [[ $state == mkdir ]]; then
+		$_comps[mkdir]
+	fi
 }
--- scripts/mkcd/mkcd_spec.sh
+++ scripts/mkcd/mkcd_spec.sh
@@ -1,52 +1,54 @@
 Describe 'mkcd'
-    Include ./mkcd.zsh
+	Include ./mkcd.zsh
 
-    BeforeEach 'setup'
-    AfterEach 'cleanup'
+	BeforeEach 'setup'
+	AfterEach 'cleanup'
 
-    setup() {
-        TMPDIR=$(mktemp -d)
-        cd "$TMPDIR" || exit 1
-    }
+	setup() {
+		TMPDIR=$(mktemp -d)
+		cd "$TMPDIR" || exit 1
+	}
 
-    cleanup() {
-        rm -rf "$TMPDIR"
-    }
+	cleanup() {
+		rm -rf "$TMPDIR"
+	}
 
-    It 'shows help with -h'
-        man() {
-            echo "man called"
-        }
-        When call mkcd -h
-        The status should be success
-        The output should include 'man called'
-    End
+	It 'should show help with -h'
+		man() {
+			echo "man called"
+		}
+		When call mkcd -h
+		The status should be success
+		The output should include 'man called'
+	End
 
-    It 'creates a single directory and enters it'
-        When call mkcd testdir
-        The status should be success
-        The path "$TMPDIR/testdir" should be a directory
-        The path $(pwd) should equal "$TMPDIR/testdir"
-    End
+	It 'should create a single directory and enter it'
+		When call mkcd testdir
+		The status should be success
+		The path "$TMPDIR/testdir" should be a directory
+		The path $(pwd) should equal "$TMPDIR/testdir"
+	End
 
-    It 'creates nested directories with -p and enters the last one'
-        When call mkcd -p nested/dir/structure
-        The status should be success
-        The path "$TMPDIR/nested/dir/structure" should be a directory
-        The path $(pwd) should equal "$TMPDIR/nested/dir/structure"
-    End
+	It 'should create multiple directories and enter the last one'
+		When call mkcd testsome testdirectories testinarow
+		The status should be success
+		The path "$TMPDIR/testsome" should be a directory
+		The path "$TMPDIR/testdirectories" should be a directory
+		The path "$TMPDIR/testinarow" should be a directory
+		The path $(pwd) should equal "$TMPDIR/testinarow"
+	End
 
-    It 'forwards options to mkdir'
-        When call mkcd -illegal testperm
-        The status should be failure
-        The path "$TMPDIR/testperm" should not be exist
-        The error should include 'illegal option'
-    End
+	It 'should fail if its missing at least 1 directory'
+		When call mkcd
+		The status should be failure
+		The error should include 'at least 1 argument'
+	End
 
-    It 'fails when mkdir fails (e.g., missing name)'
-        When call mkcd
-        The status should be failure
-        The error should include 'at least 1 argument'
-    End
+	It 'should forward options to mkdir'
+		When call mkcd -p some/nested/directory
+		The status should be success
+		The path "$TMPDIR/some/nested/directory" should be a directory
+		The path $(pwd) should equal "$TMPDIR/some/nested/directory"
+	End
 
 End
