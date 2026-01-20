bbddb801bddbf3736dd05b44edc3934df47199ae
vabatta <2137077+vabatta@users.noreply.github.com>
2026-01-20 23:38:11 +0100

    fix(dotenv): standardize CLI options format
    - Add 'General:' section grouping for general options
    - Document mode:+ parameter behavior

--- scripts/dotenv/options.sh
+++ scripts/dotenv/options.sh
@@ -11,6 +11,7 @@ append_env_file() {
 # @bundle end
 
 parser_definition() {
+	# mode:+ enables POSIX-style cumulative flag mode
 	setup REST help:usage abbr:true mode:+ -- \
 		"Usage: dotenv [options...] command [args...]"
 	msg -- '' 'Load environment from .env files and execute command' ''
@@ -18,6 +19,8 @@ parser_definition() {
 	param :append_env_file -e --env-file var:FILE init:'ENV_FILES=()' -- ".env file to load (repeatable)"
 	flag  EXEC_MODE -x --exec  -- "Replace process with exec"
 	flag  STRICT    -s --strict -- "Fail on warnings"
+	msg -- ''
+	msg -- '  General:'
 	flag  QUIET     -q --quiet -- "Suppress warnings"
 	disp  :usage    -h --help
 	disp  VERSION   -V --version
